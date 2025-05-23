b32129aa1a8159082723561af9acf0906afcbc5a
daddyjanno <jn.drugmand@gmail.com>
2025-05-23 10:53:10 +0200

    update router

--- Kasa/src/main.tsx
+++ Kasa/src/main.tsx
@@ -2,22 +2,40 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
+import About from "./pages/About";
+import React from "react";
+import MainLyout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
+    element: <MainLyout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
   },
 ]);
 
 const root = document.getElementById("root");
 
 if (root) {
-  ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 } else {
   throw new Error("Failed to find the root element");
 }
