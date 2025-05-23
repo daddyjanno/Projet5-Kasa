af74701a9999085d408edab4183827dc4eb1e472
daddyjanno <jn.drugmand@gmail.com>
2025-05-23 09:31:07 +0200

    Home page and error page router

--- Kasa/src/main.tsx
+++ Kasa/src/main.tsx
@@ -1,9 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+} else {
+  throw new Error("Failed to find the root element");
+}
