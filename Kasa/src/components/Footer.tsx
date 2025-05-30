2355a4c669a993876c912227d91a17d3d5f6f866
daddyjanno <jn.drugmand@gmail.com>
2025-05-30 08:23:18 +0200

    Footer

---
+++ Kasa/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import logo from "../assets/LOGOfooter.png";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <img src={logo} alt="logo Kasa" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
