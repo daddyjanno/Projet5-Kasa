300ceed304f588916337b8a40dffc27b4ccd9e08
daddyjanno <jn.drugmand@gmail.com>
2025-06-03 22:36:47 +0200

    mainLayout

--- Kasa/src/layouts/MainLayout.tsx
+++ Kasa/src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer/Footer";
 
 function MainLyout() {
   return (
