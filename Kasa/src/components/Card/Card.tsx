69c66f9641ece797eaa2c336c116db70901504e1
daddyjanno <jn.drugmand@gmail.com>
2025-06-06 09:56:08 +0200

    card

--- Kasa/src/components/Card/Card.tsx
+++ Kasa/src/components/Card/Card.tsx
@@ -1,17 +1,16 @@
 import styles from "./Card.module.scss";
 
-interface Props {
+interface ICard {
   title: string;
   cover: string;
 }
 
-function Card(props: Props) {
-  const { title, cover } = props;
+function Card(card: ICard) {
   return (
     <div className={styles.card}>
-      <img className={styles.img} src={cover} alt="cover" />
+      <img className={styles.img} src={card.cover} alt="cover" />
       <div className={styles.textContainer}>
-        <p className={styles.text}>{title}</p>
+        <p className={styles.text}>{card.title}</p>
       </div>
     </div>
   );
