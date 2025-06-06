354269df689eecbd58f17661b4c8d00f8685cbda
daddyjanno <jn.drugmand@gmail.com>
2025-06-06 08:49:44 +0200

    get id of rental in Rental page

--- Kasa/src/pages/Rental/Rental.tsx
+++ Kasa/src/pages/Rental/Rental.tsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function Rental() {
+  const { id } = useParams();
   return (
     <>
       <h1>Rental page</h1>
+      <p>id: {id}</p>
     </>
   );
 }
