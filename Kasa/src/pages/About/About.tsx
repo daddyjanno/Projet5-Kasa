fc1eaa24e28f5d4d8e9412e9143f4c6e4a8c843e
daddyjanno <jn.drugmand@gmail.com>
2025-06-09 07:07:28 +0200

    About

--- Kasa/src/pages/About/About.tsx
+++ Kasa/src/pages/About/About.tsx
@@ -1,8 +1,23 @@
+import Banner from "../../components/Banner/Banner";
+import styles from "./About.module.scss";
+import AboutPageBanner from "../../assets/background-about.png";
+import informations from "../../datas/informations.json";
+import Collapse from "../../components/Collapse/Collapse";
+
 function About() {
   return (
-    <>
-      <h1>A propos</h1>
-    </>
+    <div className={styles.container}>
+      <Banner src={AboutPageBanner} />
+      {informations.map((item, index) => (
+        <div key={index} className={styles.wrapper}>
+          <Collapse
+            key={`${index} - ${item}`}
+            title={item.title}
+            text={item.description}
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
