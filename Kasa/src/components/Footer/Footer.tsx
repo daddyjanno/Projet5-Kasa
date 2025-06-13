cd59e19c00ccb083f6ac7673cf70f31826a1ae71
daddyjanno <jn.drugmand@gmail.com>
2025-06-13 10:02:38 +0200

    Footer

--- Kasa/src/components/Footer/Footer.tsx
+++ Kasa/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <img src={logo} alt="logo Kasa" className={styles.container} />
+        <img src={logo} alt="logo Kasa" />
         <p>© 2020 Kasa. All rights reserved</p>
       </div>
     </footer>
