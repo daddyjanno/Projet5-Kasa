28d7e72831406a02b2f82f1fdf0427c1b9207992
daddyjanno <jn.drugmand@gmail.com>
2025-06-13 10:03:01 +0200

    Error page

--- Kasa/src/pages/ErrorPage/ErrorPage.tsx
+++ Kasa/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./Error.module.scss";
+
 function ErrorPage() {
   return (
-    <>
-      <h1>Error page</h1>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.text}>
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className={styles.link} to={"/"}>
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   );
 }
 
