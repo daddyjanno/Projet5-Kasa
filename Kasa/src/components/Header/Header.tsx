d19340d884631afd021bc48b741ecfe283e22db2
daddyjanno <jn.drugmand@gmail.com>
2025-06-17 08:40:09 +0200

    change class for function in header

--- Kasa/src/components/Header/Header.tsx
+++ Kasa/src/components/Header/Header.tsx
@@ -1,39 +1,36 @@
 import { NavLink } from "react-router";
 import logo from "../../assets/LOGOmain.png";
-import { Component, type ReactNode } from "react";
 import styles from "./Header.module.scss";
 
-class Header extends Component {
-  render(): ReactNode {
-    return (
-      <header className={styles.header}>
-        <img
-          src={logo}
-          alt="logo Kasa"
-          id="mainLogo"
-          className={styles.mainLogo}
-        />
-        <nav className={styles.nav}>
-          <NavLink
-            to={"/"}
-            className={({ isActive }) =>
-              `${styles.navLink} ${isActive ? styles.active : ""}`
-            }
-          >
-            Accueil
-          </NavLink>
-          <NavLink
-            to={"about"}
-            className={({ isActive }) =>
-              `${styles.navLink} ${isActive ? styles.active : ""}`
-            }
-          >
-            A propos
-          </NavLink>
-        </nav>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className={styles.header}>
+      <img
+        src={logo}
+        alt="logo Kasa"
+        id="mainLogo"
+        className={styles.mainLogo}
+      />
+      <nav className={styles.nav}>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `${styles.navLink} ${isActive ? styles.active : ""}`
+          }
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to={"about"}
+          className={({ isActive }) =>
+            `${styles.navLink} ${isActive ? styles.active : ""}`
+          }
+        >
+          A propos
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
