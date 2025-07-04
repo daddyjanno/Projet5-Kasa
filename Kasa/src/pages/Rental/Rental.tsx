80d354f1498603c1540c59c4565c762b1d6d2eea
daddyjanno <jn.drugmand@gmail.com>
2025-07-04 08:56:19 +0200

    rental

--- Kasa/src/pages/Rental/Rental.tsx
+++ Kasa/src/pages/Rental/Rental.tsx
@@ -2,6 +2,8 @@ import { Navigate, useParams } from "react-router-dom";
 import rentalsList from "../../datas/logements.json";
 import styles from "./Rental.module.scss";
 import Slideshow from "../../components/Slideshow/Slideshow";
+import Tag from "../../components/Tag/Tag";
+import Avatar from "../../components/Avatar/Avatar";
 
 function Rental() {
   const rentalId = useParams();
@@ -14,6 +16,16 @@ function Rental() {
         <div className={styles.titleContainer}>
           <h2 className={styles.title}>{data.title}</h2>
           <h3 className={styles.location}>{data.location}</h3>
+          <div className={styles.tags}>
+            {data.tags.map((tag, index) => (
+              <Tag key={`${index}-${tag}`} tag={tag} />
+            ))}
+          </div>
+        </div>
+        <div className={styles.avatarAndRatingContainer}>
+          <div className={styles.avatarContainer}>
+            <Avatar host={data.host} />
+          </div>
         </div>
       </div>
     </div>
