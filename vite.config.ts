f0d631cdc84d487242dafc164a6c37f585d96927
daddyjanno <jn.drugmand@gmail.com>
2025-08-01 09:12:21 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Projet5-Kasa",
+  base: "/Projet5-Kasa/",
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    open: "/Projet5-Kasa/",
+  },
 });
